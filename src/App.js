2b4b1edb1665c0f72807afe238b8addd34759b8e
Vikas Singh <vikassinghbhadauria@gmail.com>
2022-08-06 17:17:52 -0500

    Deploy on Git hub

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+function App() {
+  return (
+    <React.Fragment>
+      <title>Ravikas</title>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = props =>{
+
+    return(
+    <React.Fragment>
+        <header>
+            <ul>
+                <li>Home</li>
+                <li>About US</li>
+                <li>Contact US</li>
+                <li>Blog</li>
+            </ul>
+        </header>
+    </React.Fragment>
+    )
+}
+export default Header;

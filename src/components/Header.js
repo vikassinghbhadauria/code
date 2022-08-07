73f07ee74a4bb12d3b36712a34a1f8b92499442d
Vikas Singh <vikassinghbhadauria@gmail.com>
2022-08-06 19:34:24 -0500

    Adding header.css file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Header = props =>{
 
     return(
     <React.Fragment>
-        <header>
-            <ul>
-                <li>Home</li>
-                <li>About US</li>
-                <li>Contact US</li>
-                <li>Blog</li>
+        <header >
+            <ul className={classes.header}>
+                <li >Home</li>
+                <li >About US</li>
+                <li >Contact US</li>
+                <li >Blog</li>
             </ul>
         </header>
     </React.Fragment>

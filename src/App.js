22629ccaa2559c399ee0fc47b09b641bd2586407
Vikas Singh <vikassinghbhadauria@gmail.com>
2022-08-06 20:35:56 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Body from './components/Body';
+
 function App() {
   return (
     <React.Fragment>
       <title>Ravikas</title>
       <Header />
+      <Body></Body>
     </React.Fragment>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Body.module.css'
+
+const Body =() => {
+
+    return(
+        <React.Fragment className={classes.body}>
+            <h2>this is body</h2>
+        </React.Fragment>
+    )
+
+}
+
+export default Body;

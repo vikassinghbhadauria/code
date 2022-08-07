d8b5f119eac0030d667269760f88353ace3f247e
Vikas Singh <vikassinghbhadauria@gmail.com>
2022-08-06 21:06:12 -0500

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,11 @@ const Header = props =>{
                 <li><a href="http://localhost:3000/">About US</a></li>
                 <li><a href="http://localhost:3000/">Contact US</a></li>
                 <li><a href="http://localhost:3000/">Blog</a></li>
-                           
+                <div className={classes.container}>
+                <div className={classes.bar1}></div>
+                <div className={classes.bar2}></div>
+                <div className={classes.bar3}></div>           
+                </div>
             </ul>
         </header>
     </React.Fragment>
